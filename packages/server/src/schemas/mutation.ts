a1eb83c7468c414d1c3223ef7f989048dffcd825
Samuel Durante <samueldurante.h@gmail.com>
2022-07-04 18:33:37 -0300

    Fixing some things

--- packages/server/src/schemas/mutation.ts
+++ packages/server/src/schemas/mutation.ts
@@ -1,11 +1,13 @@
 import { GraphQLObjectType } from 'graphql';
 
 import * as userMutations from '../modules/user/mutations';
+import * as postMutations from '../modules/post/mutations';
 
 export const mutation = new GraphQLObjectType({
   name: 'Mutation',
   description: 'Root mutation',
   fields: () => ({
-    ...userMutations
+    ...userMutations,
+    ...postMutations
   })
 });
