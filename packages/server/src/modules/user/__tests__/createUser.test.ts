839a453121ea4101ef7ec4b6b6ce128d81fd5fa6
Samuel Durante <samueldurante.h@gmail.com>
2022-07-05 16:54:38 -0300

    Testing user creation

---
+++ packages/server/src/modules/user/__tests__/createUser.test.ts
@@ -0,0 +1,39 @@
+import { graphql } from 'graphql'
+
+import { schema } from '../../../schemas/schema'
+import { connectWithMongoose, disconnectWithMongoose } from '../../../../test'
+
+beforeAll(connectWithMongoose)
+afterAll(disconnectWithMongoose)
+
+it('should create a new user', async () => {
+  const mutation = `
+    mutation M($username: String!, $password: String!) {
+      userRegisterMutation(input: { username: $username, password: $password }) {
+        token
+        me {
+          id
+          username
+        }
+      }
+    }
+  `
+
+  const result = await graphql({
+    schema,
+    source: mutation,
+    rootValue: {},
+    variableValues: {
+      username: 'samuel',
+      password: 'f3ad452ba4a69a91d0acfb64038f54a5',
+    },
+  })
+
+  expect(result.errors).toBeUndefined()
+
+  // TODO: improve this
+  const user = result?.data?.userRegisterMutation as any
+
+  expect(user.token).toBeDefined()
+  expect(user.me.id).toBeDefined()
+})
